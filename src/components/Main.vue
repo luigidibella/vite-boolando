cc09d7edeb150443b4d946d8caffae6622128338
luigidibella <lykos.omega@gmail.com>
2024-03-21 21:44:35 +0100

    [add] main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,12 +5,25 @@
 </script>
 
 <template>
-  <div>
-    <h1>Main</h1>
-  </div>
+  <main>
+    <div class="container">
+      <h1>Questo è il <span>Main</span>.</h1>
+    </div>
+  </main>
 </template>
 
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+.container{
+  height: calc(100vh - 70px - 80px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span{
+    color: $bg-header;
+  }
+}
 
 </style>
